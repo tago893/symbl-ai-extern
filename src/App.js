d15a7309e81837663183f867830059ccf2e39c04
VARUN CHIKKALA <varun.chikkala2002@gmail.com>
2021-12-19 20:37:37 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function GetSortOrder(prop){
+  return function(a,b){
+    if( a[prop] > b[prop]){
+      return 1;
+    }else if( a[prop] < b[prop] ){
+      return -1;
+    }
+    return 0;
+  }
+}
+function getRequest(data) {
+  try {
+    data.sort(GetSortOrder("id"));
+    for (var obj in data) {
+      console.log(data[obj]['id'] + " " +data[obj]['name']);
+    }
+    return data;
+  } catch (e) {
+    if (Number(e.status) === 404) {
+      return [];
+    }
+    console.error("error in getRequest", JSON.stringify(e.errors));
+    throw e;
+  }
+}
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+  componentDidMount() {
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos'; 
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ data: getRequest(data)});
+        });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+        
+        <div>
+
+        <h1> React app to fetch and show sorted data </h1>
+          {
+            data.map((item) => (
+              <ol key={ item.id }>
+                { item.id } : { item.name }
+              </ol>
+          ))
+        }
+        </div>
+    )}
 }
 
 export default App;
